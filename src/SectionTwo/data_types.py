e80fed62d434841eb284511e1c290b2fa23a309a
jgvlf <joaogabrielvlf@gmail.com>
2024-08-26 16:24:30 -0300

    Adding "Data Types" script.

---
+++ src/SectionTwo/data_types.py
@@ -0,0 +1,22 @@
+# Numbers types
+number = 100 # int
+percent = 0.50 # float
+imaginary = 9j # complex
+
+# Booleans types
+is_connected = True # bool or Literal[True]
+has_money = False # bool or Literal[False]
+
+# String Types
+text = 'Hello Bob!' # str or Literal['Hello Bob!']
+
+# Sequence Types
+numbers = [1, 2, 3, 4] # list[int]
+coordinates = (2.5, 1.0) # tuple[float, float]
+
+# Mapping Types
+users = {"Mario": 1, "Luigi": 2} # dict[str, int]
+
+# Set Types
+raffle = {1, 10, 25, 50} # set[int]
+frozen = frozenset({1, 2, 3}) #frozenset[int]

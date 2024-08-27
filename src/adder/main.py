438efffb66ef5325526b94e5100530fe5db7232e
jgvlf <joaogabrielvlf@gmail.com>
2024-08-27 13:44:02 -0300

    Adding "Adder" program

---
+++ src/adder/main.py
@@ -0,0 +1,7 @@
+print("Welcome to your simple adder!")
+print("-----------------------------")
+
+a: str = input('Enter the value of "a": ')
+b: str = input('Enter the value of "b": ')
+
+print("The result is:", int(a) + int(b))

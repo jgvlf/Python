0abe66d115519104ce5b4547a032289b2d7e3087
jgvlf <joaogabrielvlf@gmail.com>
2024-08-27 08:33:28 -0300

    Adding "Operators One" script.

---
+++ src/SectionTwo/operators_one.py
@@ -0,0 +1,26 @@
+a: int = 2
+b: int = 4
+
+print(a // b)
+print(4 // 3)
+print(5 // 3)
+print(a**b)
+print(3 % 2)
+print(9 % 7)
+print(5 % 2)
+print(5 // 2)
+
+
+print(10 + 10 * 2)
+print((10 + 10) * 2)
+
+x: float = 2.0
+
+x += 2.0  # x = x + 2
+x -= 2.0  # x = x - 2
+x /= 2.0  # x = x / 2
+x *= 2.0  # x = x * 2
+x //= 2.0  # x = x // 2
+x **= 2.0  # x = x ** 2
+x %= 2.0  # x = x % 2
+print(x)

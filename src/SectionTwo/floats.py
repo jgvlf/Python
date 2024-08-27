cac59a6cdbab5c903f3009e61a874a44d175034d
jgvlf <joaogabrielvlf@gmail.com>
2024-08-27 08:21:49 -0300

    Adding "Floats" script.

---
+++ src/SectionTwo/floats.py
@@ -0,0 +1,16 @@
+PI: float = 3.1415
+percent: float = 0.5
+height_in_metres: float = 1.72
+
+
+a: float = 0.5
+b: float = 1.5
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+number: float = 10  # 10.0
+number2: int = 1.0  # warning: float can't be a integer.

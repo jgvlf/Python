d84a96970491e155ea4bf4731c892e4940d9bd59
jgvlf <joaogabrielvlf@gmail.com>
2024-09-04 09:50:30 -0300

    Creating "Is only Letters" project.
    This project apply the "raise" and "try...except" features.

---
+++ src/letters_only/main.py
@@ -0,0 +1,28 @@
+import string
+
+
+def is_only_letters(text: str) -> None:
+    alphabet: str = (
+        string.ascii_letters + "ãÃõÕâÂêÊîÎôÔûÛáÁéÉíÍóÓúÚàÀèÈìÌòÒùÙ" + "çÇ" + " "
+    )
+    for char in text:
+        if char not in alphabet:
+            raise ValueError("The text isn't contain only letters...")
+    print(f'"{text}" is only letters, good job!')
+
+
+def main() -> None:
+    while True:
+        try:
+            user_input: str = input("Check the text: ")
+            is_only_letters(user_input)
+        except ValueError:
+            print("Please only enter a Portuguese letters...")
+        except Exception as e:
+            print(
+                f"Found a unexpected error: \nRepresentation: {repr(e)}; \nType: {type(e)}; \nValue: {e}"
+            )
+
+
+if __name__ == "__main__":
+    main()

e0466cd6d5827452b094a65588a2181fb201f294
jgvlf <joaogabrielvlf@gmail.com>
2024-09-05 11:21:43 -0300

    Resolve "grocery_list" issues that was been proposed.

--- src/grocery_list/main.py
+++ src/grocery_list/main.py
@@ -1,6 +1,14 @@
 import sys
 
 
+def default_exception_msg(e: Exception) -> None:
+    print(
+        f"Unexpected error {e};"
+        + f"\nType: {type(e)};"
+        + f"\nRepresentation: {repr(e)}."
+    )
+
+
 def welcome_message() -> None:
     print("Welcome to Groceries!")
     print("Enter: ")
@@ -8,8 +16,16 @@ def welcome_message() -> None:
     print("1 - To add an item")
     print("2 - To remove an item")
     print("3 - To list all items")
+    print("4 - To edit a list item")
     print("0 - To exit the program")
     print("-" * 20)
+
+
+def verify_input_item(item: str) -> bool:
+    if item:
+        return True
+    else:
+        return False
 
 
 def add_item(item: str, groceries: list[str]) -> None:
@@ -23,6 +39,20 @@ def remove_item(item: str, groceries: list[str]) -> None:
         print(f'"{item}" has been removed!')
     except ValueError:
         print(f'NO "{item}" found in: {groceries}')
+    except Exception as e:
+        default_exception_msg(e)
+
+
+def item_exists(item: str, groceries: list[str]) -> bool:
+    if item in groceries:
+        return True
+    else:
+        return False
+
+
+def edit_item(item: str, new_item: str, groceries: list[str]) -> None:
+    groceries[groceries.index(item)] = new_item
+    print(f'"{item}" was changed by "{new_item}"')
 
 
 def display(groceries: list[str]) -> None:
@@ -34,7 +64,7 @@ def display(groceries: list[str]) -> None:
 
 
 def is_an_option(text: str) -> bool:
-    return text in ["1", "2", "3", "0"]
+    return text in ["1", "2", "3", "4", "0"]
 
 
 def main() -> None:
@@ -49,6 +79,9 @@ def main() -> None:
 
         if user_input == "1":
             new_item: str = input("What item would you like to add? >> ").lower()
+            if not verify_input_item(new_item):
+                print("Please, enter a valid item...")
+                continue
             add_item(new_item, groceries)
         elif user_input == "2":
             item_to_removed: str = input(
@@ -57,6 +90,16 @@ def main() -> None:
             remove_item(item_to_removed, groceries)
         elif user_input == "3":
             display(groceries)
+        elif user_input == "4":
+            if len(groceries) == 0:
+                print("No grocery registered...")
+                continue
+            existed_item: str = input("What item would you like to change? >> ").lower()
+            if not item_exists(existed_item, groceries):
+                print(f'The "{existed_item}" doesn\'t exist...')
+                continue
+            item: str = input("What is the new item name? >> ").lower()
+            edit_item(existed_item, item, groceries)
         elif user_input == "0":
             print("Exiting program...")
             sys.exit(0)
